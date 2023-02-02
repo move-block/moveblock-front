2aa62d48acb2399ac737e797fa8f8d5df9b1e747
Hudson Jang <narraba3@gmail.com>
2023-02-03 01:34:16 +0900

    fix : fix execution modal css (#13)

--- src/stacks/components/ExecutionModal.tsx
+++ src/stacks/components/ExecutionModal.tsx
@@ -1,7 +1,5 @@
-import {CheckCircleTwoTone, CloseCircleTwoTone, CloseOutlined, CopyOutlined} from '@ant-design/icons';
+import {CheckCircleFilled, CheckCircleOutlined, CloseOutlined, CopyOutlined} from '@ant-design/icons';
 import {Button, Input, Modal} from 'antd';
-
-
 
 const FunctionModal = ({
   isOpen,
@@ -21,7 +19,7 @@ const FunctionModal = ({
       footer={null}
       onCancel={onClose}
       closeIcon={<div />}
-      width="30%"
+      width="25%"
       className="overflow-auto"
     >
       <div className="flex flex-col gap-4">
@@ -32,16 +30,17 @@ const FunctionModal = ({
         </div>
         <div className="flex flex-col items-center">
           <h4>
-            <CheckCircleTwoTone twoToneColor="#597EF7" />
+            <CheckCircleFilled className="text-3xl text-primary"/>
           </h4>
-          <p>Transaction Success!</p>
+          <p className="text-4xl font-bold">Transaction</p>
+          <p className="text-4xl font-bold">Success!</p>
         </div>
         <div>
           <a href={`https://explorer.aptoslabs.com/txn/${transactionHash}`}
              target="_blank" rel="noreferrer">https://explorer.aptoslabs.com/txn/{transactionHash}</a>
         </div>
         <div className="flex flex-row">
-          <h4>Byte code</h4>
+          <p className="items-center text-lg">Byte code</p>
           <Button type="ghost" icon={<CopyOutlined />} />
         </div>
         <Input.TextArea
