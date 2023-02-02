aa18fd1361794fa46716e2e93c7e49d82fc7682c
helloheesu <heesu.jung.developer@gmail.com>
2023-02-03 01:49:20 +0900

    fix: integrate wallet adapter
    connect state hasn't been synced

--- src/common/components/Gnb.tsx
+++ src/common/components/Gnb.tsx
@@ -3,12 +3,9 @@ import { Header } from 'antd/lib/layout/layout';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
 import LogoImage from '~public/logo_white.png';
 import ConnectWallet from './ConnectWallet';
-import { PetraWallet } from 'petra-plugin-wallet-adapter';
-import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
-const wallets = [new PetraWallet()];
+
 const menuItems = [
   {
     href: '/modules',
@@ -25,41 +22,38 @@ const Gnb = () => {
 
   return (
     <Header className="max-sm:px-2 flex items-center justify-center gap-4 h-[110px]">
-      <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
-        <Link href="/">
-          <Image src={LogoImage} alt="Move Block logo" />
-        </Link>
-        <Menu
-          className="bg-transparent m-0 min-w-0 flex-auto"
-          mode="horizontal"
-          theme="dark"
-          selectedKeys={[router.pathname]}
-          items={[
-            ...menuItems.map(({ href, label }) => ({
-              key: href,
-              label: (
-                <Link href={href}>
-                  <span className="text-h5">{label}</span>
-                </Link>
-              ),
-            })),
-            {
-              key: 'help',
-              label: (
-                <a
-                  href="https://www.notion.so/Move-Block-9deb30eef70240b5bf86634f06716ffd"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="text-h5">Help</span>
-                </a>
-              ),
-            },
-          ]}
-        />
-
-        <ConnectWallet responsive />
-      </AptosWalletAdapterProvider>
+      <Link href="/">
+        <Image src={LogoImage} alt="Move Block logo" />
+      </Link>
+      <Menu
+        className="bg-transparent m-0 min-w-0 flex-auto"
+        mode="horizontal"
+        theme="dark"
+        selectedKeys={[router.pathname]}
+        items={[
+          ...menuItems.map(({ href, label }) => ({
+            key: href,
+            label: (
+              <Link href={href}>
+                <span className="text-h5">{label}</span>
+              </Link>
+            ),
+          })),
+          {
+            key: 'help',
+            label: (
+              <a
+                href="https://www.notion.so/Move-Block-9deb30eef70240b5bf86634f06716ffd"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="text-h5">Help</span>
+              </a>
+            ),
+          },
+        ]}
+      />
+      <ConnectWallet responsive />
     </Header>
   );
 };
